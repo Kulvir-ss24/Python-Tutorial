407b8f42c50e6f415f9808a1b64b043062f9752f
Kulvir Singh Saggu <105497797+Kulvir-ss24@users.noreply.github.com>
2025-08-04 20:36:33 +0530

    Initial commit: Add variables_datatypes.py

---
+++ variables_datatypes.py
@@ -0,0 +1,11 @@
+print("it's kulvir here")   # where print is function 
+ 
+#what is variable?
+# A variable is a name that refers to a value. it can change its value.
+name="kulvir"
+age=20
+print(name,age)
+
+#now let's create another variable and store the existing variable
+name2 = name
+print(name2)  # This will print the value of name2, which is "kulvir"
