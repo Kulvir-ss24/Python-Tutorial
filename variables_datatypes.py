33a3d56dd2fdf37544e89e3251b2fc6ccb937df2
Kulvir Singh Saggu <105497797+Kulvir-ss24@users.noreply.github.com>
2025-08-04 21:37:54 +0530

    Initial commit: Add variables_datatypes.py

--- variables_datatypes.py
+++ variables_datatypes.py
@@ -1,11 +1,45 @@
 print("it's kulvir here")   # where print is function 
  
-#what is variable?
+#What is variable?
 # A variable is a name that refers to a value. it can change its value.
 name="kulvir"
 age=20
 print(name,age)
 
-#now let's create another variable and store the existing variable
+#Now let's create another variable and store the existing variable
 name2 = name
 print(name2)  # This will print the value of name2, which is "kulvir"
+
+#Rules for Identifiers commonly known as variable names and can be called as functions.
+# 1. Variable names can only contain letters, numbers, and underscores.
+# 2. Variable names cannot start with a number.
+# 3. Identifiers can be the combination of uppercase and lowercase letters and can be of any length.
+# For example, these are valid identifiers:
+user_name = "Alice"
+userAge = 25
+_user_id = 12345
+user123 = "Bob"
+
+# 4. Identifiers cannot be a reserved keyword in Python (like 'if', 'else', 'while', etc.).
+myVariable = 10  # Valid variable name
+print_name = "Hello"  # Valid variable name
+your_first_name = "Kulvir"  # Valid variable name
+
+# 5. it cannot contain special characters like @, #, $, %, etc.
+# Identifier cannot start with a digit. Some examples of invalid variable names:
+# 1st_variable = 5  # Invalid variable name (starts with a digit)
+# my-variable = 10  # Invalid variable name (contains a hyphen)
+# my variable = 10  # Invalid variable name (contains a space)
+
+# 6. Variable names are case-sensitive, meaning 'myVariable' and 'myvariable' are different variables.
+MyVariable = 20  # Valid variable name, different from myVariable
+variable_1 = 30  # Valid variable name
+variable1= 40  # Valid variable name
+
+# 7. Variable names should be descriptive and meaningful to improve code readability.
+# For example, instead of using 'x' or 'y', use 'user_age' or 'product_price'.
+user_age = 30  # Descriptive variable name
+product_price = 19.99  # Descriptive variable name
+print(user_age,product_price)
+# 8. Variable names should be simple, short and meaningful.
+
